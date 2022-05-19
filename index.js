61371b73e9d9af7ec3e1859ff36dbd07f3913b8d
Luis Felipe <luis.felipe.lima23@gmail.com>
2022-05-19 18:23:26 -0300

    Continuação do nodeJS

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
-function pegaArquivo(caminho){
-
+function trataErro(erro) {
+  throw new Error(chalk.red(erro));
 }
+
+function pegaArquivo(caminho) {
+  const encoding = 'utf-8';
+  fs.readFile(caminho, encoding, (erro, texto) => {
+    if (erro) {
+      trataErro(erro);
+    }
+    console.log(chalk.green(texto));
+  });
+}
+
+pegaArquivo('./arquivos/texto1.md');
